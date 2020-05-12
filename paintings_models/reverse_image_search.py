d715f15c113df0e316eea9aa92cfb73cdcb1e23d
kubapi <kuba.sliz@hotmail.com>
2020-05-12 18:40:43 +0200

    Running RASTA model for inference with successful outcome!

--- paintings_models/reverse_image_search.py
+++ paintings_models/reverse_image_search.py
@@ -1,6 +1,11 @@
 import os
 os.environ["KMP_WARNINGS"] = "FALSE"
 
+import logging
+import tensorflow as tf
+logging.getLogger('tensorflow').disabled = True
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -24,54 +29,52 @@ from keras.models import Model, load_model
 from load_images import get_image_path_by_label
 
 #local path to files
-path_extracted_features = "data/extracted_features.npy"
-path_lsa_features = "data/lsa_features.npy"
-path_pca_features = "data/pca_features.npy"
-path_rasta_model = "data/rasta_model.h5"
+path_extracted_features = "data/ext_feat/extracted_features_"
+
+path_dim_red_features = "data/dim_red/features_"
+
+path_rasta_model = "data/models/rasta_model.h5"
 
 def load_VGG16_feature_extractor():
     model = keras.applications.VGG16(weights='imagenet', include_top=True)
-    #returns last before output layer that serves as a feature extractor
-    return Model(inputs=model.input, outputs=model.get_layer("fc2").output)
+    name = "vg116"
+    #returns penultimate layer that serves as a feature extractor
+    return name, Model(inputs=model.input, outputs=model.get_layer("fc2").output)
 
 def load_RASTA_feature_extractor():
     model = load_model(path_rasta_model)
-    return Model(inputs=model.input, output=model.get_layer("add_16").output)
+    name = "rasta"
+    #returns penultimate layer that serves as a feature extractor
+    return name, Model(inputs=model.input, output=model.get_layer("global_average_pooling2d_1").output)
 
-def clear_data():
-    '''
-    Removes features extracted for vgg16, PCA and LSA data
-    '''
-    os.remove(path_extracted_features_vgg16)
-    os.remove(path_lsa_features)
-    os.remove(path_pca_features)
-
-def pre_extract_features(X):
+def feature_extraction(X, loaded_feature_extractor):
     '''
     Extracts features using pre-trained model, as input accepts feature_extractor (VGG16 default) and numpy array of images
     '''
     print("Starting feature extraction")
-    feature_extractor = load_RASTA_feature_extractor()
-    if os.path.exists(path_extracted_features):
+    name, feature_extractor = loaded_feature_extractor
+    if os.path.exists(path_extracted_features+name+".npy"):
         print("Pre-traiend features found!")
         return feature_extractor, np.load(path_extracted_features)
     else:
         pre_features = [feature_extractor.predict(np.expand_dims(x, 0))[0] for x in tqdm(X)]
-        return feature_extractor, np.array(pre_features)
+        np.save(path_extracted_features+name+".npy", pre_features)
+        return name, feature_extractor, np.array(pre_features)
 
-def get_models_and_features(pre_features, lsa_n_components = 100, pca_n_components = 50):
+def get_models_and_features(name ,pre_features, lsa_n_components = 100, pca_n_components = 50):
     '''
     Extracts features using PCA, LSA methods as a final extraction
     Returns [lsa_model, lsa_features, pca_model, pca_features]
     '''
     print("Extracting PCA and LSA features!")
+
     lsa = TruncatedSVD(n_components=lsa_n_components, algorithm='randomized')
     lsa_features = lsa.fit_transform(pre_features)
-    np.save(path_lsa_features, lsa_features)
+    np.save(path_dim_red_features+"lsa_"+name+".npy", lsa_features)
 
     pca = PCA(n_components=pca_n_components)
     pca_features = pca.fit_transform(pre_features)
-    np.save(path_pca_features, pca_features)
+    np.save(path_dim_red_features+"pca_"+name+".npy", lsa_features)
 
     return lsa, lsa_features, pca, pca_features
 
@@ -93,11 +96,11 @@ if __name__ == '__main__':
     X = np.load('data/X_data.npy')
     labels = np.load('data/labels_data.npy')
 
-    # loading extractor and applying extraction
+    # # loading extractor and applying extraction
     feature_extractor, pre_features = pre_extract_features(X)
 
-    #loading and using pca and lsa
+    # #loading and using pca and lsa
     lsa, lsa_features, pca, pca_features = get_models_and_features(pre_features)
-    # assert "8745170876.jpg" in reverse_search("data/test.jpg", feature_extractor, pca, pca_features, 5, labels)[0] == True, "Problem with reverse search!"
-    print(reverse_search("data/test.jpg", feature_extractor, pca, pca_features, 5, labels))
-    # print(reverse_search("king.jpg", feature_extractor, pca, pca_features, 5, labels))
+
+
+    # print(reverse_search("data/test.jpg", feature_extractor, pca, pca_features, 5, labels))
